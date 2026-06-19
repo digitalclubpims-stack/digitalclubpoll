a0c84ce520f122c26029b7629bdd23f043b506c6
digitalclubpims-stack <digitalclub.pims@gmail.com>
2026-06-19 13:16:39 +0530

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,1 +1,56 @@
+import { db } from "./firebase.js";
 
+import {
+addDoc,
+collection,
+getDocs
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const btn =
+document.getElementById("createEventBtn");
+
+const list =
+document.getElementById("eventsList");
+
+if(btn){
+
+btn.addEventListener("click", async ()=>{
+
+const eventName =
+document.getElementById("eventName").value;
+
+await addDoc(
+collection(db,"events"),
+{
+name:eventName,
+status:"draft",
+createdAt:new Date()
+}
+);
+
+alert("Event Created");
+
+location.reload();
+
+});
+
+}
+
+async function loadEvents(){
+
+if(!list) return;
+
+const snapshot =
+await getDocs(collection(db,"events"));
+
+snapshot.forEach(doc=>{
+
+list.innerHTML +=
+`<p>${doc.data().name}</p>`;
+
+});
+
+}
+
+loadEvents();
