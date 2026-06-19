4f2680256486e844c866edf0e466ad4e35d3161e
digitalclubpims-stack <digitalclub.pims@gmail.com>
2026-06-20 01:15:13 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,27 @@
-// Firebase SDK Imports
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+// ==========================================
+// FIREBASE CONFIGURATION
+// ==========================================
 
-// Firebase Configuration
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import { getStorage }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+
+
+// ==========================================
+// PIMS POLL FIREBASE CONFIG
+// ==========================================
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCsuOcZ86B55VnOXQA4Pa3O03n2XQn1_Rk",
+
+    apiKey: "AIzaSyCsuOcZ86B55VnOXQA4Pa3O03n2XQn1_Rk",
   authDomain: "pims-poll.firebaseapp.com",
   databaseURL: "https://pims-poll-default-rtdb.firebaseio.com",
   projectId: "pims-poll",
@@ -13,14 +29,29 @@ const firebaseConfig = {
   messagingSenderId: "14637440444",
   appId: "1:14637440444:web:3444173f31a6c43a809fb9",
   measurementId: "G-FP42MFB2WR"
+
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-// Services
-const auth = getAuth(app);
-const db = getFirestore(app);
+// ==========================================
+// INITIALIZE FIREBASE
+// ==========================================
 
-// Export Services
-export { auth, db };
+const app =
+initializeApp(firebaseConfig);
+
+
+// ==========================================
+// EXPORT SERVICES
+// ==========================================
+
+export const auth =
+getAuth(app);
+
+export const db =
+getFirestore(app);
+
+export const storage =
+getStorage(app);
+
+export default app;
