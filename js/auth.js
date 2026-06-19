cfe82b6a8241c5b6e320100f9cd13b1349e42f21
digitalclubpims-stack <digitalclub.pims@gmail.com>
2026-06-19 13:04:15 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,37 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword }
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+const loginBtn = document.getElementById("loginBtn");
+
+if (loginBtn) {
+    loginBtn.addEventListener("click", async () => {
+
+        const email =
+            document.getElementById("email").value;
+
+        const password =
+            document.getElementById("password").value;
+
+        try {
+
+            const userCredential =
+                await signInWithEmailAndPassword(
+                    auth,
+                    email,
+                    password
+                );
+
+            alert("Login Successful");
+
+            window.location.href =
+                "admin.html";
+
+        } catch (error) {
+
+            alert(error.message);
+
+        }
+
+    });
+}
